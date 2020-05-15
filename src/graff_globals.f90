dc547fc2c62dbd357dfca8bb8d0e44796929e376
James Tappin <jtappin@gmail.com>
2020-05-15 16:55:51 +0100

    Tabs converted

--- src/graff_globals.f90
+++ src/graff_globals.f90
@@ -31,9 +31,9 @@ module graff_globals
 
   ! Widgets that may be needed outside the GUI-specifics
 
-  type(c_ptr) :: gr_window = c_null_ptr		! The top level window
-  type(c_ptr) :: gr_drawing_area = c_null_ptr	! The main plotting surface
-  type(c_ptr) :: gr_infobar = c_null_ptr	! Messages window
+  type(c_ptr) :: gr_window = c_null_ptr         ! The top level window
+  type(c_ptr) :: gr_drawing_area = c_null_ptr   ! The main plotting surface
+  type(c_ptr) :: gr_infobar = c_null_ptr        ! Messages window
   type(c_ptr) :: xhair_but = c_null_ptr         ! Cross hairs toggle
 
 
