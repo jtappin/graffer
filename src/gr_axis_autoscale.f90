899c9445551803793f01b96d14a82fdf6ea9c4f2
James Tappin <jtappin@gmail.com>
2021-01-29 11:33:37 +0000

    Fix scaling of Y-axes when secondary is present.

--- src/gr_axis_autoscale.f90
+++ src/gr_axis_autoscale.f90
@@ -103,7 +103,7 @@ contains
           else
              call gr_autoscale_x_polar(data, axmin, axmax, vis_only)
           end if
-       else
+       else if (data%y_axis == axis-2) then
           if (data%mode == 0) then
              call gr_autoscale_y_rect(data, axmin, axmax, vis_only)
           else
